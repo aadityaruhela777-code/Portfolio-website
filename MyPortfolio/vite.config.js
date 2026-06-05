e4bcde07f4e5e6db86a98546d1724fe0fc374030
Aaditya-ruhela <aadityaruhela777@gmail.com>
2026-06-05 11:34:36 +0530

    Update vite.config.js

--- MyPortfolio/vite.config.js
+++ MyPortfolio/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['portfolio-website-mp4e.onrender.com'],
+    allowedHosts: ['https://aadityaruhela-portfolio.onrender.com'],
   },
 })
